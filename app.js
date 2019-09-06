2543f4cc6e21531b648db980786af7ec78d2cc4f
Chang Xiong <cnxiong03@gmail.com>
2019-09-05 21:04:14 -0400

    changing text and HTML content

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const wmf = document.querySelector("book-list li:nth-ChannelSplitterNode(2) .name");
-console.log(wmf);
+const books = document.querySelectorAll("#book-list li .name");
 
-var books = document.querySelector("#book-list li .name");
-console.log(books);
-
-books = document.querySelectorAll("#book-list li .name");
-console.log(books);
-
-Array.from(book).forEach(function(book){
-    console.log(book);
+Array.from(books).forEach(function(book){
+    book.textContent += " (Book title)";
 });
+
+const bookList = document.querySelector("#book-List");
+bookList.innerHTML = "<h2>Books and more books...</h2>";
+bookList.innerHTML = "<p>This is how you add HTML content</p>";
